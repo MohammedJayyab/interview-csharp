bc19f35ff2bd5badafa4906f5b785958a4592826
MohammedJayyab <mohabujayyab@gmail.com>
2024-05-22 00:04:59 +0200

    - Creating ShortUrlCreationException class

---
+++ src/Application/Common/Exceptions/ShortUrlCreationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortenerService.Application.Common.Exceptions;
+
+public class ShortUrlCreationException : Exception
+{
+    /// <summary>
+    /// This exception is thrown when the application command can not create a short URL in the database.
+    /// </summary>
+    /// <param name="message">The message describes the error.</param>
+    public ShortUrlCreationException(string message)
+        : base(message)
+    {
+    }
+}
