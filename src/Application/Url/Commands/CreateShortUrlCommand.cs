a3a5cf120106857bed7192280239daea949cc3af
MohammedJayyab <mohabujayyab@gmail.com>
2024-05-22 00:28:02 +0200

    - Move CreateShortUrlCommandValidator to a separate file - And adding some validation rules to it.

--- src/Application/Url/Commands/CreateShortUrlCommand.cs
+++ src/Application/Url/Commands/CreateShortUrlCommand.cs
@@ -11,16 +11,6 @@ public record CreateShortUrlCommand : IRequest<string>
     public string Url { get; init; } = default!;
 }
 
-public class CreateShortUrlCommandValidator : AbstractValidator<CreateShortUrlCommand>
-{
-    public CreateShortUrlCommandValidator()
-    {
-        _ = RuleFor(v => v.Url)
-          .NotEmpty()
-          .WithMessage("Url is required.");
-    }
-}
-
 public class CreateShortUrlCommandHandler : IRequestHandler<CreateShortUrlCommand, string>
 {
     private readonly IUrlRepository _urlRepository;
---
+++ src/Application/Url/Commands/CreateShortUrlCommandValidator.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidation;
+
+namespace UrlShortenerService.Application.Url.Commands;
+
+public class CreateShortUrlCommandValidator : AbstractValidator<CreateShortUrlCommand>
+{
+    private const int MAX_URL_LENGTH = 2000;
+
+    public CreateShortUrlCommandValidator()
+    {
+        _ = RuleFor(v => v.Url)
+             .NotEmpty()
+             .WithMessage("Url is required.")
+
+             .Must(IsValidUrl)
+             .WithMessage("Invalid URL format.")
+
+             .MaximumLength(MAX_URL_LENGTH)
+             .WithMessage($"URL is too long. Maximum length is '{MAX_URL_LENGTH}' characters.")
+
+             .Must(HasValidScheme)
+             .WithMessage("URL must start with a valid scheme like: (http, https).");
+
+        /*.Must(NoSpecialCharacters)
+          .WithMessage("URL contains invalid characters.");
+        */
+    }
+
+    private bool IsValidUrl(string url)
+    {
+        return Uri.TryCreate(url, UriKind.Absolute, out _);
+    }
+
+    private bool HasValidScheme(string url)
+    {
+        if (Uri.TryCreate(url, UriKind.Absolute, out var uriResult))
+        {
+            return uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps;
+        }
+
+        return false;
+    }
+
+    private bool NoSpecialCharacters(string url)
+    {
+        var disallowedCharacters = new[]
+        {
+                ' ', '<', '>', '"', '{', '}', '|', '\\', '^', '`'
+        };
+
+        return !url.Any(c => disallowedCharacters.Contains(c));
+    }
+}
