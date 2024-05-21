d9b5d9d9ab1a618d17058734b50fa42e762f9267
MohammedJayyab <mohabujayyab@gmail.com>
2024-05-22 01:30:14 +0200

    - Create RedirectToUrlCommandHandler.cs - MoveRedirectToUrlCommand to a separate file

--- src/Application/Url/Commands/RedirectToUrlCommand.cs
+++ src/Application/Url/Commands/RedirectToUrlCommand.cs
@@ -1,50 +1,7 @@
-﻿using FluentValidation;
-using HashidsNet;
-using MediatR;
-using UrlShortenerService.Application.Common.Exceptions;
-using UrlShortenerService.Application.Common.Interfaces;
+﻿using MediatR;
 
 namespace UrlShortenerService.Application.Url.Commands;
-
 public record RedirectToUrlCommand : IRequest<string>
 {
     public string Id { get; init; } = default!;
-}
-
-public class RedirectToUrlCommandHandler : IRequestHandler<RedirectToUrlCommand, string>
-{
-    private readonly IUrlRepository _urlRepository;
-    private readonly IHashids _hashids;
-
-    public RedirectToUrlCommandHandler(IUrlRepository urlRepository, IHashids hashids)
-    {
-        _urlRepository = urlRepository;
-        _hashids = hashids;
-    }
-
-    public async Task<string> Handle(RedirectToUrlCommand request, CancellationToken cancellationToken)
-    {
-        var decodedId = DecodeSingleIdFromRequest(request);
-        var url = await _urlRepository.FindUrlByIdAsync(decodedId, cancellationToken);
-
-        if (url == null || url.OriginalUrl == null)
-        {
-            throw new NotFoundException($"No URL found for request Id: {decodedId}.");
-        }
-
-        return url.OriginalUrl;
-    }
-
-    private long DecodeSingleIdFromRequest(RedirectToUrlCommand request)
-    {
-        var decodedIds = _hashids.DecodeLong(request.Id);
-        var decodedId = decodedIds.FirstOrDefault();
-
-        if (decodedId == 0)
-        {
-            throw new NotFoundException($"Request Id: {request.Id} not fond.");
-        }
-
-        return decodedId;
-    }
 }
